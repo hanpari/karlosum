290be135b372920a88923e7eaefd41bc6e478397
hanpari <hanpari@seznam.cz>
2020-04-06 09:09:18 +0200

    Test files and test added

--- test/KarlosumTests.cs
+++ test/KarlosumTests.cs
@@ -1,12 +1,17 @@
+using System.IO;
+using System.Reflection;
 using System;
 using Xunit;
 using Karlosum;
-
+using static System.Environment;
 
 namespace test
 {
     public class UnitTest1
     {
+
+
+
         [Fact]
         public void Basic()
         {
@@ -14,5 +19,16 @@ namespace test
             Assert.Equal("0000", bs.ToHexadecimalString());
 
         }
+
+        [Theory]
+        [InlineData("harmless.txt")]
+        [InlineData("executable.exe")]
+        [InlineData("recursive/deeper_file.dat")]
+        public void TestFilesExist(string filename)
+        {
+            string path = "../../../testdir/files/" + filename;
+            Assert.True(File.Exists(path));
+        }
+
     }
 }
